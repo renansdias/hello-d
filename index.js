5291f618527eff3f7b17291347a61fdafea4a6c6
Renan Dias <renan@flypay.co.uk>
2016-09-28 15:44:29 +0100

    Upgraded to version 1.0.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = 4567;
 var service = "D";
-var version = "2.0.0";
+var version = "1.0.0";
 
 app.get('/', function(req, res) {
     console.log("Someone is requesting access to version: " + version + " of service " + service);
